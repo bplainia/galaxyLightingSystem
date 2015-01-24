9d053c20bb2ef1cb528844f6db13c8215d53955e
Benjamin <bplain3118@students.pcci.edu>
2015-01-23 22:42:41 -0600

    Initial Commit after creating the MPLabX project with some basic code
    Signed-off-by: Benjamin <bplain3118@students.pcci.edu>

---
+++ include/solarLight.h
@@ -0,0 +1,7 @@
+#ifndef _SOLARLIGHT_
+#define _SOLARLIGHT_
+
+void interrupt low_priority isr_low();
+void interrupt high_priority isr_high();
+
+#endif
---
+++ src/solarLight.c
@@ -0,0 +1,18 @@
+#include "include/solarLight.h"
+
+
+// TODO: need config bits and the xc8 header!
+void main(void)
+{
+    while(1) continue; // loop forever
+}
+
+void interrupt low_priority isr_low()
+{
+   ;
+}
+
+void interrupt high_priority isr_high()
+{
+    ;
+}
